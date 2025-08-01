f832b6a9c72fa0d58ad9a81464ee31753a6d2558
juzdalua <bearwcw@naver.com>
2025-08-01 09:44:06 +0900

    Add DEBUG libs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import Debug from 'debug';
 import { basename } from 'path';
+import * as bcrypt from 'bcrypt'
+
 const debug = Debug(`app:${basename(__dirname)}:${basename(__filename)}`);
 console.log(`app:${basename(__dirname)}:${basename(__filename)}`)
 
@@ -10,5 +12,8 @@ async function bootstrap() {
   await app.listen(3000);
 
   debug(123)
+  
+  const pwd = await bcrypt.hash('78xhdtls@', 10);
+  console.log(pwd)
 }
 bootstrap();
